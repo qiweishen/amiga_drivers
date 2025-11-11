9f47562680dd98380079bb45571e6747bc738037
qiweishen <qiwei-shen@outlook.com>
2025-11-11 06:42:55 +0000

    Refining

--- ins401_ros2/ins401_driver/Start.bash
+++ ins401_ros2/ins401_driver/Start.bash
@@ -1,2 +1,2 @@
-sudo setcap cap_net_raw+ep ./build/INS401_Driver
-./build/INS401_Driver
+sudo setcap cap_net_raw+ep /mnt/managed_home/farm-ng-user-qiweishen/workspace/ins401_ros2/ins401_ros2/ins401_driver/build/INS401_Driver
+/mnt/managed_home/farm-ng-user-qiweishen/workspace/ins401_ros2/ins401_ros2/ins401_driver/build/INS401_Driver
